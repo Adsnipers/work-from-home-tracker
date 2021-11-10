c7200561937dc1d408dafbdf4d77f4f15d45890e
Adsnipers <adsnipers@outlook.com>
2021-11-10 11:31:57 +1000

    init

---
+++ main.py
@@ -0,0 +1,45 @@
+# Employee Work from home tracker - By Ashton Southall
+
+def addEmployeeHours():
+    print("Add Employee Hours")
+    print("~~~~~~~~~~~~~~~~~~")
+    print()
+
+def showHoursWorkedReport():
+    print("Hours Worked Report")
+    print("~~~~~~~~~~~~~~~~~~~")
+    print()
+
+#Main Menu
+#Show program title
+print("---------------------------------------------")
+print("    Welcome to the work from home tracker    ")
+print("              DIAMOND REALTY                 ")
+print("---------------------------------------------")
+print("")
+
+# Set menu option variable
+choice = 1
+
+while (choice !=3):
+    # Display menu of options
+    print("Please select one of the following options to continue")
+    print("1. Enter hours")
+    print("2. Produce Report")
+    print("3. Exit")
+    print()
+
+    # Get option
+    choice = int(input("Please enter selection: "))
+
+    # Check user choice
+    
+    if (choice == 1):
+        print("Selected: 1")
+    elif (choice == 2):
+        print("selected 2")
+    elif (choice == 3):
+        print("Selected 3")
+    else:
+        print("Please input only one of the 3 options. (1, 2 or 3)")
+    
